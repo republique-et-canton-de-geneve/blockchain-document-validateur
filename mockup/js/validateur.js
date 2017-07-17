575c5c25e71ad7e050adbd0aa3a7f324e5e2dfdc
6l20 <s@6120.eu>
2017-07-17 12:18:57 +0200

    Final validator for pre-demo

--- mockup/js/validateur.js
+++ mockup/js/validateur.js
@@ -1,8 +1,35 @@
 Dropzone.autoDiscover = false;
+var myDropzone;
+function successmultiple(files, message, e) {
+    from = message.from;
+    target_hash = message.target_hash;
+    t = new Date(message.time * 1000);
+    from_link = '<a href="https://ropsten.etherscan.io/address/' + from + '">' + from + '</a>';
+    display_str = "Horodaté le " + t + " par " + from_link + " (RCGE)<br/>Hash fichier: " + target_hash;
+    $("#infobox").html(display_str);
+    $("#infobox").attr("class", "alert alert-success");
+    for (i = 0; i < files.length; i++)
+        myDropzone.removeFile(files[i]);
+}
+function errormultiple(files, message, e) {
+    $("#infobox").text(message);
+    $("#infobox").attr("class", "alert alert-danger");
+}
 $(function() {
-    $("div#validatezone").dropzone({
-        url : "/api/validate",
-        uploadMultiple: true,
-        paramName: "myfiles"
-    });
+  myDropzone = new Dropzone("div#validatezone", {
+    url : "/api/validate",
+    uploadMultiple: true,
+    paramName: "myfiles",
+    dictDefaultMessage: "Cliquez ici ou déplacer l'extrait(.pdf) et son reçu (.json)",
+    dictFallbackMessage: "Cliquez ici ou déplacer l'extrait(.pdf) et son reçu (.json)",
+    maxFile: 2,
+    autoProcessQueue: false,
+    successmultiple: successmultiple,
+      errormultiple: errormultiple,
+      addRemoveLinks: true,
+  });
 });
+
+function processValidate() {
+    myDropzone.processQueue();
+}
