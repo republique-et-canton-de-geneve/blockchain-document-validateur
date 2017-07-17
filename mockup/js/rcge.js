c3000c9c421161099620e67889824d33ebc0f9b0
6l20 <s@6120.eu>
2017-07-17 12:20:05 +0200

    Final horodateur for pre-demo

--- mockup/js/rcge.js
+++ mockup/js/rcge.js
@@ -5,6 +5,7 @@ angular.module('rc',[])
   $scope.numLimit = 16;
   $http.get(endpoint + '/horodatage').
     success(function(data, status, headers, config) {
+      data.reverse();
       $scope.extract_list = data;
       console.log(data);
     }).
@@ -25,4 +26,41 @@ function getParameterByName(name, url) {
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+Dropzone.autoDiscover = false;
+var uploadextraitzone;
+function successmultiple(files, message, e) {
+    from = message.from;
+    target_hash = message.target_hash;
+    t = new Date(message.time * 1000);
+    display_str = "En attente d'ancrage dans un block Ethereum";
+    $("#infobox").html(display_str);
+    $("#infobox").attr("class", "alert alert-success");
+    setTimeout(function () {
+        location.reload();
+    }, 25000);
+}
+function errormultiple(files, message, e) {
+    $("#infobox").text(message);
+    $("#infobox").attr("class", "alert alert-danger");
+    console.log(message);
+}
+$(function() {
+  uploadextraitzone = new Dropzone("div#uploadextraitzone", {
+    url : "/api/upload",
+    uploadMultiple: true,
+    paramName: "myfiles",
+    dictDefaultMessage: "Cliquez ici ou déplacer les extraits au format PDF",
+    dictFallbackMessage: "Cliquez ici ou déplacer les extraits au format PDF",
+    maxFile: 2,
+    autoProcessQueue: false,
+    successmultiple: successmultiple,
+      errormultiple: errormultiple,
+      addRemoveLinks: true,
+  });
+});
+
+function processValidate() {
+    uploadextraitzone.processQueue();
 }
