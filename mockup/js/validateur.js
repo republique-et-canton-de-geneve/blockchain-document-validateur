d3ee808699e9b0a6c55af8b65619fcd90bb80761
Magicking <s@6120.eu>
2017-08-18 02:20:33 +0200

    Fixed endpoint to be relative + changing infobox

--- mockup/js/validateur.js
+++ mockup/js/validateur.js
@@ -1,27 +1,28 @@
+var endpoint = 'api';
 Dropzone.autoDiscover = false;
 var myDropzone;
+var messageValide = "Documents valides";
+var messageInvalide = "Documents non valides";
+
 function successmultiple(files, message, e) {
-    from = message.from;
-    target_hash = message.target_hash;
-    t = new Date(message.time * 1000);
-    from_link = '<a href="https://ropsten.etherscan.io/address/' + from + '">' + from + '</a>';
-    display_str = "Horodaté le " + t + " par " + from_link + " (RCGE)<br/>Hash fichier: " + target_hash;
-    $("#infobox").html(display_str);
     $("#infobox").attr("class", "alert alert-success");
+    $("#iconbox").attr("class", "fa fa-check fa-stack-1x fa-inverse");
+    $("#msgbox").html(messageValide);
     for (i = 0; i < files.length; i++)
         myDropzone.removeFile(files[i]);
 }
 function errormultiple(files, message, e) {
-    $("#infobox").text(message);
     $("#infobox").attr("class", "alert alert-danger");
+    $("#iconbox").attr("class", "fa fa-exclamation fa-stack-1x fa-inverse");
+    $("#msgbox").html(messageInvalide);
 }
 $(function() {
   myDropzone = new Dropzone("div#validatezone", {
-    url : "/api/validate",
+    url : endpoint + "/validate",
     uploadMultiple: true,
     paramName: "myfiles",
-    dictDefaultMessage: "Cliquez ici ou déplacer l'extrait(.pdf) et son reçu (.json)",
-    dictFallbackMessage: "Cliquez ici ou déplacer l'extrait(.pdf) et son reçu (.json)",
+    dictDefaultMessage: "Téléverser votre fichier ainsi que son reçu (fichier avec une extension .json).",
+    dictFallbackMessage:"Téléverser votre fichier ainsi que son reçu (fichier avec une extension .json).",
     maxFile: 2,
     autoProcessQueue: false,
     successmultiple: successmultiple,
