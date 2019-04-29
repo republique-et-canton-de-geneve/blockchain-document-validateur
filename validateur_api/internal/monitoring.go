518dd1d82caf9fc55b65621843414b34c6a44068
Ubuntu <tom.chataigner@yahoo.fr>
2019-04-29 06:27:49 +0000

    :sparkles: Added dynamic tx hash for monitoring

--- validateur_api/internal/monitoring.go
+++ validateur_api/internal/monitoring.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	//"time"
 	"log"
+	"os"
 )
 
 type Sonde struct {
@@ -32,7 +33,9 @@ func GetNodeSignal(ctx context.Context) bool {
 	//txHash:= common.HexToHash("75139f2e9f045987f67ab04541d03d7cd872e663b5efd758c20da42c89e652eb")
 	//Above this comment is the line of code used for production version, the hash used is from the main net.
 	//Below this comment is the line of code used for development version, the hash used is from the rinkeby testnet.
-	txHash := common.HexToHash("d3851f8ee9bbd79a4cf332999a89a4b2c6b8d5c4c0c001ea85e95ab7997843c0")
+	txHex := os.Getenv("TX_HEX")
+
+	txHash := common.HexToHash(txHex)
 	_, _, err := blkCtx.NC.GetTransaction(ctx, txHash)
 	if err != nil {
 		return false
