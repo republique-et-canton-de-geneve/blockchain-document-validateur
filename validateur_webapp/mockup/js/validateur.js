a197eb12acb2c86ef00bc71ab120ec3ddf8b29dd
tchataigner <tom.chataigner@yahoo.fr>
2019-09-17 08:37:04 +0000

    :sparkles: Manage csrf in front end

--- validateur_webapp/mockup/js/validateur.js
+++ validateur_webapp/mockup/js/validateur.js
@@ -9,39 +9,50 @@ function successmultiple(files, message, e) {
     $("#iconbox").attr("class", "fa fa-check fa-stack-1x fa-inverse");
     $("#msgbox").html(messageValide);
 }
+
 function errormultiple(files, message, e) {
     console.log(files, message, e)
     $("#infobox").attr("class", "alert alert-danger");
     $("#iconbox").attr("class", "fa fa-exclamation fa-stack-1x fa-inverse");
-    if(message.includes("Invalid number of file") || message.includes("Invalid receipt file") || message.includes("mismatch the file hash")) {
+    if (message.includes("Invalid number of file") || message.includes("Invalid receipt file") || message.includes("mismatch the file hash")) {
         $("#msgbox").html(messageInvalide);
     } else {
         $("#msgbox").html(messageServerError);
     }
 
 }
-$(function() {
-  myDropzone = new Dropzone("div#validatezone", {
-    url : endpoint + "/validate",
-    uploadMultiple: true,
-    paramName: "myfiles",
-    dictDefaultMessage: dictDefaultMessage,
-    dictFallbackMessage:dictDefaultMessage,
-    maxFile: 2,
-    autoProcessQueue: false,
-    successmultiple: successmultiple,
-      errormultiple: errormultiple,
-      addRemoveLinks: true,
-    init: function() {
-        this.on("removedfile", function() {
-            $("#infobox").attr("class", "alert alert-info");
-            $("#iconbox").attr("class", "fa fa-info fa-stack-1x fa-inverse");
-            $("#msgbox").html(dictDefaultMessage);
-        });
-  }
-  });
+
+$(document).ready(function () {
+    console.log("alllloooooo")
+    $.get('./token', function (response) {
+        localStorage.setItem('csrfToken', response.token);
+    });
+});
+
+$(function () {
+    myDropzone = new Dropzone("div#validatezone", {
+        url: endpoint + "/validate",
+        uploadMultiple: true,
+        headers: {'Access-Control-Allow-Credentials': true},
+        paramName: "myfiles",
+        dictDefaultMessage: dictDefaultMessage,
+        dictFallbackMessage: dictDefaultMessage,
+        maxFile: 2,
+        autoProcessQueue: false,
+        successmultiple: successmultiple,
+        errormultiple: errormultiple,
+        addRemoveLinks: true,
+        init: function () {
+            this.on("removedfile", function () {
+                $("#infobox").attr("class", "alert alert-info");
+                $("#iconbox").attr("class", "fa fa-info fa-stack-1x fa-inverse");
+                $("#msgbox").html(dictDefaultMessage);
+            });
+        }
+    });
 });
 
 function processValidate() {
+    myDropzone.options.headers['X-CSRF-Token'] = localStorage.getItem("csrfToken");
     myDropzone.processQueue();
 }
