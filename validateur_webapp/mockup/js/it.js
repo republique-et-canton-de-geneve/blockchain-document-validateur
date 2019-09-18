78157f31926a6dcf0808b30e823421e50a2fae4f
tchataigner <tom.chataigner@yahoo.fr>
2019-09-18 22:47:55 +0000

    :sparkles: Added js file to manage traduction

---
+++ validateur_webapp/mockup/js/it.js
@@ -0,0 +1,4 @@
+var messageValide = "Documenti validi";
+var messageInvalide = "Documenti non validi";
+var messageServerError = "Errore del server. Per favore ritorna più tardi";
+var dictDefaultMessage = "Caricare contemporaneamente i file dell’estratto digitale in formato PDF e la ricevuta PDF.";
